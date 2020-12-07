a5b497f0008e8ffc9645a6fd93da9b688c87fd42
weslynSouza <wesley.i476@gmail.com>
2020-12-07 10:31:48 -0300

    05-bytebank

---
+++ ByteBank/05-ByteBank/ContaCorrente.cs
@@ -0,0 +1,43 @@
+﻿
+namespace _05_ByteBank
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int agencia;
+        public int numero;
+        public double saldo;
+
+        public bool Sacar(double valor)
+        {
+            if (this.saldo >= valor)
+            {
+                this.saldo -= valor;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void Depositar(double valor)
+        {
+            this.saldo += valor;
+        }
+
+        public bool Transferir(double valor, ContaCorrente contadestino)
+        {
+            if(this.saldo < valor)
+                return false;
+ 
+
+            this.saldo -= valor;
+            contadestino.Depositar(valor);
+            return true;
+
+        }
+    }
+
+}
---
+++ ByteBank/05-ByteBank/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cliente gabriela = new Cliente();
+            //gabriela.nome = "Gabriela";
+            //gabriela.cpf = "123.456.789.00";
+            //gabriela.profissao = "Arquiteta";
+
+            ContaCorrente contaGabriela = new ContaCorrente();
+            contaGabriela.titular = new Cliente();
+            contaGabriela.titular.nome = "Gabriela";
+            contaGabriela.titular.cpf = "123.456.789.00";
+            contaGabriela.titular.profissao = "Arquiteta";
+
+            Console.WriteLine("Titular da conta: " + contaGabriela.titular.nome);
+
+            Console.ReadLine();
+        }
+    }
+}
