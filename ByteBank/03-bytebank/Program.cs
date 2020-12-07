5e2e318fc3ade6c1486232e4213fc2a786cb1856
weslynSouza <wesley.i476@gmail.com>
2020-12-06 22:28:46 -0300

    03-bytebank

---
+++ ByteBank/03-bytebank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_bytebank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
+            contaDaGabriela.titular = "Gabriela";
+            contaDaGabriela.agencia = 863;
+            contaDaGabriela.numero = 863452;
+
+            Console.WriteLine(contaDaGabriela.titular === contaDaGabrielaCosta.titular);
+
+            Console.ReadLine();
+        }
+    }
+}
