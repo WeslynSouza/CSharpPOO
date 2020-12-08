b4941578383111e22d5cb7a32e8d03ee902eb21a
weslynSouza <wesley.i476@gmail.com>
2020-12-08 17:02:37 -0300

    Herança e Polimorfismo

---
+++ ByteBank-pt2/ByteBank/Program.cs
@@ -0,0 +1,42 @@
+﻿using ByteBank.Funcionarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Carlos";
+            carlos.CPF = "555.444.333.22";
+            carlos.Salario = 1200.90;
+
+            gerenciador.Registrar(carlos);
+
+            Console.WriteLine(carlos.Nome);
+            Console.WriteLine(carlos.GetBonificacao());
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "111.222.333.44";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine(gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+        }
+    }
+}
