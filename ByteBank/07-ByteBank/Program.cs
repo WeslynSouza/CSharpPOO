6dab3477e00092925f0b42db71472e9981a25a2f
weslynSouza <wesley.i476@gmail.com>
2020-12-08 10:28:03 -0300

    07-bytebank

---
+++ ByteBank/07-ByteBank/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(867, 867239);
+            ContaCorrente conta2 = new ContaCorrente(867, 867239);
+            ContaCorrente conta3 = new ContaCorrente(867, 867239);
+
+            Console.WriteLine(ContaCorrente.TotalDeContas);
+
+            Console.ReadLine();
+        }
+    }
+}
